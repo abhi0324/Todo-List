30f3ea81db90a4d2b03c4ee48bbf8099d90e12f0
abhi0324 <136226198+abhi0324@users.noreply.github.com>
2024-10-05 17:26:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+let todos = [];
+
+        function addTodo() {
+
+            todos.push({ title: document.querySelector("input").value });
+            document.querySelector("input").value = "";
+            render();
+
+        }
+
+        function deleteTodo(index) {
+            todos.splice(index, 1);
+            render();
+        }
+
+        function editTodo() {
+
+        }
+
+        function createTodoComponent(tod, index) {
+            const div = document.createElement("div");
+            const divel = document.createElement("div");
+            const divbut = document.createElement("div");
+            const check = document.createElement("input");
+            const label = document.createElement("label");
+            label.setAttribute("for","checkb");
+            label.innerHTML = tod.title;
+            check.setAttribute("type","checkbox");
+            check.setAttribute("id","checkb");
+            divel.setAttribute("id", "hello");
+            divel.appendChild(check);
+            divel.appendChild(label);
+            const delbutton = document.createElement("button");
+            delbutton.innerHTML = "Delete";
+            const editbutton = document.createElement("button");
+            delbutton.setAttribute("onclick", "deleteTodo(" + index + ")");
+            divbut.appendChild(delbutton);           
+            div.setAttribute("class","eldiv");
+            div.appendChild(divel);
+            div.appendChild(divbut);
+            
+            return div;
+        }
+
+        function render() {
+            document.querySelector("#todo").innerHTML = "";
+            for (let i = 0; i < todos.length; i++) {
+                const element = createTodoComponent(todos[i], i);
+                document.querySelector("#todo").appendChild(element);
+            }
+        }
